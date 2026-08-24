ff8051a2d5af0f2dd428d8b80f67059578e2c569
Leif Maisch <leif.maisch@icloud.com>
2026-08-24 18:12:53 +0200

    header

--- src/features/catalog/components/catalog-header.tsx
+++ src/features/catalog/components/catalog-header.tsx
@@ -17,9 +17,7 @@ export function CatalogHeader() {
         <CatalogBreadcrumbs />
       </div>
       <div className="flex shrink-0 items-center gap-1 sm:gap-2">
-        <div className="hidden sm:block">
-          <PrimaryColorSelect />
-        </div>
+        <PrimaryColorSelect />
         <ThemeToggle />
       </div>
     </header>
